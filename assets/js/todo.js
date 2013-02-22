b44c0afcfb242c9c23d6492e0f695bd61b82c570
Keith Walsh <keith.walsh@adtegrity.com>
2013-02-21 21:25:52 -0500

    Using localStorage. Needs to be refactored.

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,13 +1,13 @@
 function TodoCtrl($scope) {
   localStorage['todos'] === undefined ? $scope.todos = [] : $scope.todos = JSON.parse(localStorage['todos']);
-  $scope.archivedtodos = [];
+  localStorage['archivedtodos'] === undefined ? $scope.archivedtodos = [] : $scope.archivedtodos = JSON.parse(localStorage['archivedtodos']);
  
   $scope.addTodo = function() {
     localStorage['todoscounter'] === undefined ? nextId = 1 : nextId = localStorage['todoscounter']++;
     var todo = {id:nextId, text:$scope.todoText, done:false};
     $scope.todos.push(todo);
     $scope.todoText = '';
-    $scope.setStorage();
+    $scope.setToDoStorage();
     return false;
   };
  
@@ -29,18 +29,43 @@ function TodoCtrl($scope) {
 				$scope.archivedtodos.push(todo);
       }
     });
+    localStorage['todos'] = JSON.stringify($scope.todos);
+    localStorage['archivedtodos'] = JSON.stringify($scope.archivedtodos);
   };  
+
+  $scope.done = function(todo) {
+    if(todo.done) {
+      todos = JSON.parse(localStorage['todos']);
+      $.each(todos, function(){
+        if(this.id == todo.id) {
+          this.done = true;
+        };
+      });
+      localStorage['todos'] = JSON.stringify(todos);
+    } else{
+      todos = JSON.parse(localStorage['todos']);
+      $.each(todos, function(){
+        if(this.id == todo.id) {
+          this.done = false;
+        };
+      });
+      localStorage['todos'] = JSON.stringify(todos);
+    };
+  };
 
   $scope.unarchive = function(todo) {
 		$scope.todos.push(todo);
+    localStorage['todos'] = JSON.stringify($scope.todos);
 		$scope.archivedtodos.pop(todo);
+    localStorage['archivedtodos'] = JSON.stringify($scope.archivedtodos);
 	};
 
   $scope.deleteArchive = function(todo) {
     $scope.archivedtodos.pop(todo);
+    localStorage['archivedtodos'] = JSON.stringify($scope.archivedtodos);
   };
 
-  $scope.setStorage = function() {
+  $scope.setToDoStorage = function() {
     localStorage['todos'] = JSON.stringify($scope.todos);
     localStorage['todoscounter'] === undefined ? localStorage['todoscounter'] = 2 : "";
   };
